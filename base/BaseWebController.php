7f1c004a854aec812bdb4960da4edd8e5be744b8
Timur Sasin <sasintimur@gmail.com>
2019-06-10 00:24:43 +0300

    Day model added. Previous visited page saves in session.

--- base/BaseWebController.php
+++ base/BaseWebController.php
@@ -3,10 +3,25 @@
 
 namespace app\base;
 
+use yii\web\BadRequestHttpException;
 use yii\web\Controller;
+use Yii;
 
 
 class BaseWebController extends Controller
 {
+
+    /**
+     * @param $action
+     * @return bool
+     * @throws BadRequestHttpException
+     */
+    public function beforeAction($action)
+    {
+
+        Yii::$app->session->set('previousUrl', Yii::$app->request->referrer ?: Yii::$app->homeUrl);
+
+        return parent::beforeAction($action);
+    }
 
 }
